736135fe26a4cc3047e3930c18d9b71b9253510f
julia-ctp <juliactp.santos@gmail.com>
2025-12-10 02:38:34 -0300

    feat: integrate contact form with api

---
+++ src/service/contact.service.ts
@@ -0,0 +1,18 @@
+import type { AxiosInstance } from "axios";
+import { api } from "@/config/axios.config";
+import type { ContactFormInput } from "@/schemas/contact.schema";
+
+export class ContactService {
+  private instance: AxiosInstance = api;
+
+  async postContact(data: ContactFormInput) {
+    try {
+      const response = await this.instance.post("/contact", data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export const ContactServiceInstance = new ContactService();
